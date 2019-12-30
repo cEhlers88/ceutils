a30d271f4706005de53199b9e9893f819617d7a9
Christoph Ehlers <Christoph.Ehlers1988@gmx.de>
2019-12-30 20:56:27 +0100

    lint conform

--- src/__tests__/Datahandler.ts
+++ src/__tests__/Datahandler.ts
@@ -1,18 +1,18 @@
 import Datahandler from "../Datahandler";
 
-test('Basictest',()=>{
-    const curDatahandler = new Datahandler();
+test("Basictest", () => {
+  const curDatahandler = new Datahandler();
 
-    curDatahandler.setData('isok?','isOk!');
+  curDatahandler.setData("isok?", "isOk!");
 
-    expect(curDatahandler.getData('isok?')).toBe('isOk!');
+  expect(curDatahandler.getData("isok?")).toBe("isOk!");
 });
 
-test('Proxytest',()=>{
-    const curDatahandler = new Datahandler();
-    // @ts-ignore
-    curDatahandler.lala="OK";
+test("Proxytest", () => {
+  const curDatahandler = new Datahandler();
+  // @ts-ignore
+  curDatahandler.lala = "OK";
 
-    // @ts-ignore
-    expect(curDatahandler.lala).toBe('OK');
+  // @ts-ignore
+  expect(curDatahandler.lala).toBe("OK");
 });
