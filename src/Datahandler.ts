bae7b2c70a01bef1ac6584c7e8e3e6a7f1d0eb8c
Christoph Ehlers <Christoph.Ehlers1988@gmx.de>
2019-12-30 20:52:34 +0100

    first draft Datahandler implemented

---
+++ src/Datahandler.ts
@@ -0,0 +1,43 @@
+
+export default class {
+    private data:Array<{
+        key:string,
+        value:any
+    }>=[];
+
+    constructor() {
+        const self = this;
+        return new Proxy(this,{
+            get: (target:any, p:any)=> {
+                if (typeof target[p] !== 'undefined') return target[p];
+                return this.getData(p);
+            },
+            set(target: any, p: string | number | symbol, value: any, receiver: any): boolean {
+                if (typeof target[p] !== 'undefined') return target[p]=value;
+                self.setData((typeof p==='string'?p:'_object'),value);
+                return true;
+            }
+        })
+    }
+    public clearData():void{
+        this.data=[];
+    }
+    public getData(key:string):any{
+        const entry:{key:string,value:any}|undefined=this.data.find((entry:{key:string,value:any})=>entry.key===key);
+        return (entry?entry.value:undefined);
+    }
+    public getDataIndex(key:string):number|undefined{
+        let result:number|undefined=undefined;
+        for(let index:number=0;index<this.data.length;index++){
+            if(this.data[index].key===key){result=index;break;}
+        }
+        return result;
+    }
+    public setData(key:string, value:any){
+        const index:number|undefined=this.getDataIndex(key);
+        if(index!==undefined){this.data[index].value=value;}else{
+            this.data.push({key,value});
+        }
+        return this;
+    }
+}
---
+++ src/__tests__/Datahandler.ts
@@ -0,0 +1,18 @@
+import Datahandler from "../Datahandler";
+
+test('Basictest',()=>{
+    const curDatahandler = new Datahandler();
+
+    curDatahandler.setData('isok?','isOk!');
+
+    expect(curDatahandler.getData('isok?')).toBe('isOk!');
+});
+
+test('Proxytest',()=>{
+    const curDatahandler = new Datahandler();
+    // @ts-ignore
+    curDatahandler.lala="OK";
+
+    // @ts-ignore
+    expect(curDatahandler.lala).toBe('OK');
+});
