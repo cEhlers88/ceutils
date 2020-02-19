e8389e0e464804eaab5ba8e382d649f988ad86be
Christoph Ehlers <>
2020-02-19 21:25:35 +0100

    [COMPONENTLOADER] - implement ComponentStarter with some basic tests

---
+++ src/service/AbstractService.ts
@@ -0,0 +1,7 @@
+import IService from "../Interfaces/IService";
+
+export default abstract class AbstractService implements IService {
+    public getName(): string {
+        throw new Error("Method 'getName' not implemented.");
+    }
+}
