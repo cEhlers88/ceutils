73d8f533b2990eebe766742cae8429eb7e1594d8
Christoph Ehlers <christoph.ehlers1988@gmx.de>
2024-11-12 18:25:30 +0100

    add some exports

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,17 @@ import EventHndl from "./handler/Eventhandler";
 import { IHtmlElementExtensions } from "./Interfaces/IHtmlElementExtensions";
 import canvas from "./lib/canvas";
 import _createStore from "./lib/createStore";
+import DialogClass from "./lib/Dialog";
 import dom from "./lib/dom";
 import CachedDataProviderClass from "./Provider/CachedDataProvider";
+import DialogProviderClass  from "./service/DialogProvider";
 import ObjectStoreProviderClass from "./service/ObjectStoreProvider";
 
 export default {
   CachedDataProvider: CachedDataProviderClass,
   Datahandler: DataHndl,
+  Dialog: DialogClass,
+  DialogProvider: DialogProviderClass,
   Eventhandler: EventHndl,
   ObjectStoreProvider: ObjectStoreProviderClass,
   canvasLib: canvas,
@@ -19,8 +23,10 @@ export default {
 
 export const createStore = _createStore;
 export const CachedDataProvider = CachedDataProviderClass;
-export const Datahandler = DataHndl;
 export const canvasLib = canvas;
+export const Datahandler = DataHndl;
+export const Dialog = DialogClass;
+export const DialogProvider = DialogProviderClass;
 export const domLib = dom;
 export const Eventhandler = EventHndl;
 export const ObjectStoreProvider = ObjectStoreProviderClass;
